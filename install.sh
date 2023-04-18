e1b9dd723e703b8270dd0a68ad8fcdd072c077de
James Blume <james@crenexi.com>
2023-04-17 18:04:25 -0700

    Fix

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 # Installs all of these scripts at ~/bin
 
-$bin=~/bin
-
 scripts_names=(
   "release-start"
-  "release-finish",
-  "cxr-component",
+  "release-finish"
+  "cxr-component"
   "cxr-container"
 )
 
+# Folder copies
+sudo cp -r ./cxr-templates/ ~/bin
+
+# Script copies
 for name in "${scripts_names[@]}"
 do
-  cp ./$name.sh $bin/
-  chmod +x $bin/$name.sh
+  cp ./$name.sh ~/bin
+  chmod +x ~/bin/$name.sh
 done
-
-# Folder copies
-cp $bin/
