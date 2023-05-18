0eb1ee291cfca726c6d18e09ef91efc9cf23fc1e
James Walrath <james@crenexi.com>
2023-05-17 17:15:36 -0700

    Verify script

--- cxx/cxx-verify.sh
+++ cxx/cxx-verify.sh
@@ -4,25 +4,22 @@ numMissing=0
 
 function missing() {
     ((numMissing++))
-    printf "/!\ MISSING: $1\n"
+    echo "/!\ MISSING: $1"
 }
 
 function verify() {
     eval "which $1" || missing $1
 }
 
-printf "VERIFYING BIN\n"
-
 verify "git"
 verify "htop"
 verify "cmatrix"
 verify "curl"
 verify "wmctrl"
-verify "baobabz"
+verify "baobab"
 verify "chrome-gnome-shell"
-verify "dropbox"
 verify "spotify"
 verify "code"
 
-printf "MISSING: $numMissing\n"
-printf "DONE VERIFYING\n"
+printf "\nMISSING: $numMissing\n"
+echo "DONE VERIFYING"
