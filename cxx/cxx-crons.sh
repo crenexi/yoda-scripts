2ffbdccd167452c6904ea1b17401040ff87633b7
James Walrath <james@crenexi.com>
2023-05-22 01:24:57 -0700

    Logs

--- cxx/cxx-crons.sh
+++ cxx/cxx-crons.sh
@@ -2,16 +2,25 @@
 # Checkup on some specific cron jobs
 
 echo "#######################################"
-echo "## CRONS | ROOT #######################"
+echo "## LAST BACKUPS #######################"
 echo "#######################################"
-sudo crontab -l
-
+echo "HOME:               $(cat /etc/crenexi/crenexi@victory/bac-home_time)"
+echo "MOST:               $(cat /etc/crenexi/crenexi@victory/bac-most_time)"
+echo "GAMES:              $(cat /etc/crenexi/crenexi@victory/bac-games_time)"
+echo
 echo "#######################################"
-echo "## CRONS | CRENEXI ####################"
+echo "## SEE MORE ###########################"
 echo "#######################################"
-crontab -l
-
-echo "#######################################"
-echo "## CRONS | LOG TAIL ###################"
-echo "#######################################"
-grep -E 'CRON.*crenexi' /var/log/syslog | tail -n 300 | less -F
+echo "LAST BACKUP LOG"
+echo "  cat /etc/crenexi/crenexi@$(hostname)/bac-home.log | less -F"
+echo "  cat /etc/crenexi/crenexi@$(hostname)/bac-most.log | less -F"
+echo "  cat /etc/crenexi/crenexi@$(hostname)/bac-games.log | less -F"
+echo "CRENEXI CRON LOG"
+echo "  cat /etc/crenexi/cron.log | less -F"
+echo "ROOT/USER CRONTABS"
+echo "  sudo crontab -l"
+echo "  chrontab -l"
+echo "ALL/USER CHRON SYSLOG"
+echo "  grep -E 'CRON.' /var/log/syslog | tail -n 300 | less -F"
+echo "  grep -E 'CRON.*crenexi' /var/log/syslog | tail -n 300 | less -F"
+echo
