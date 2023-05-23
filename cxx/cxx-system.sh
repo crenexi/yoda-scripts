5bb14581e656ef4822849c6231ba1c56984d2469
James Blume <james@crenexi.com>
2023-05-22 21:37:01 -0700

    Edit for /hdd optional case

--- cxx/cxx-system.sh
+++ cxx/cxx-system.sh
@@ -8,8 +8,14 @@ ip_text=$(ip addr show | awk '/inet / && $2 !~ /^127\.0\.0\.1/ {split($2, a, "/"
 
 du_root=$(df -h --output=pcent / | awk 'NR==2{print $1}')
 du_home=$(df -h --output=pcent "$HOME" | awk 'NR==2{print $1}')
-du_hdd=$(df -h --output=pcent /hdd | awk 'NR==2{print $1}')
 du_panda=$(df -h --output=pcent /nas/Panda-Private | awk 'NR==2{print $1}')
+
+function echo_du_hdd() {
+  if [ -d "/hdd" ]; then
+    du_hdd=$(df -h --output=pcent /hdd | awk 'NR==2{print $1}')
+    echo "Disk Usage for /hdd: $du_hdd"
+  fi
+}
 
 echo "## UPTIME & USAGE #####################"
 echo "Uptime: $uptime_text"
@@ -19,7 +25,7 @@ echo "Memory Usage: $memory_usage GB"
 echo "## STORAGE ############################"
 echo "Disk Usage for /: $du_root"
 echo "Disk Usage for $HOME/: $du_home"
-echo "Disk Usage for /hdd: $du_hdd"
+echo_du_hdd
 echo "Disk Usage for /nas: $du_panda"
 
 echo "## MISC ###############################"
