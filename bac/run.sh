6e0066d184845632a601515bd38986dda6516917
James Walrath <james@crenexi.com>
2023-06-13 15:26:32 -0700

    Edited run.sh

--- bac/run.sh
+++ bac/run.sh
@@ -5,6 +5,13 @@ source /home/crenexi/.bashrc
 bac="/home/crenexi/.cx/bin/bac"
 logs="/home/crenexi/.cx/logs"
 
+# Log file
+bac_log="$logs/bac.log"
+if [ ! -f "$bac_log" ]; then
+  touch "$bac_log"
+fi
+
+# Backups
 "$bac/bac-home.sh" >> "$logs/bac.log" 2>&1
 "$bac/bac-most.sh" >> "$logs/bac.log" 2>&1
 "$bac/bac-games.sh" >> "$logs/bac.log" 2>&1
