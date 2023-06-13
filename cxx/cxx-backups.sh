c6069060d3db0d729987f3419c892fd4c5a7e04c
James Walrath <james@crenexi.com>
2023-06-13 16:57:43 -0700

    Last update

--- cxx/cxx-backups.sh
+++ cxx/cxx-backups.sh
@@ -2,16 +2,13 @@
 # Checkup on some specific cron jobs
 
 host=$(hostname)
-dir_logs="$HOME/.cx/logs"
-dir_bac="$dir_logs/bac/crenexi@${host}"
-
-logs="$HOME/.cx/logs/bac/$dir_key"
+logs="$HOME/.cx/logs"
 
 function echo_last_backups() {
   dirs=("bac-home" "bac-most" "bac-games")
 
   for dir in "${dirs[@]}"; do
-    filepath="$dir_bac/${dir}_time"
+    filepath="$logs/bac/${dir}_time"
 
     if [ -f "$filepath" ]; then
       echo "$dir: $(cat "$filepath")"
@@ -23,7 +20,7 @@ function echo_last_backups_less() {
   dirs=("bac-home" "bac-most" "bac-games")
 
   for dir in "${dirs[@]}"; do
-    filepath="$dir_bac/$dir\.log"
+    filepath="$logs/bac/$dir\.log"
 
     if [ -f "$filepath" ]; then
       echo "  $(cat "$filepath") | less -F"
@@ -37,7 +34,7 @@ echo "## SEE MORE ###################################################"
 echo "LAST BACKUP LOG"
 echo_last_backups_less
 echo "CRENEXI BACKUP LOG"
-echo "  cat $dir_logs/bac.log | less -F"
+echo "  cat $logs/bac.log | less -F"
 echo "ROOT/USER CRONTABS"
 echo "  sudo crontab -l"
 echo "  chrontab -l"
