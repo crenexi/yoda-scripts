2be5f337c7000f6de9f3760dfaf39cde3af2632d
James Walrath <james@crenexi.com>
2023-06-13 16:19:26 -0700

    Run fixes

--- bac/run.sh
+++ bac/run.sh
@@ -2,16 +2,28 @@
 # This runs all backup scripts
 source /home/crenexi/.bashrc
 
-bac="/home/crenexi/.cx/bin/bac"
-logs="/home/crenexi/.cx/logs"
+bin="/home/crenexi/.cx/bin/bac"
+log="/home/crenexi/.cx/logs/bac.log"
 
-# Log file
-bac_log="$logs/bac.log"
-if [ ! -f "$bac_log" ]; then
-  touch "$bac_log"
+function trim_log() {
+  local max_lines=100
+  local line_count=$(wc -l < "$log")
+
+  if [ "$line_count" -gt "$max_lines" ]; then
+    remove_count=$((line_count - max_lines)) # lines to remove
+    tail -n "$max_lines" "$log" > "$log.tmp" # trimmed file
+    mv "$log.tmp" "$log"
+  fi
+}
+
+# Trim or create log
+if [ -f "$log" ]; then
+  trim_log
+else
+  touch "$log"
 fi
 
 # Backups
-"$bac/bac-home.sh" >> "$logs/bac.log" 2>&1
-"$bac/bac-most.sh" >> "$logs/bac.log" 2>&1
-"$bac/bac-games.sh" >> "$logs/bac.log" 2>&1
+"$bin/bac-home.sh" >> "$log" 2>&1
+"$bin/bac-most.sh" >> "$log" 2>&1
+"$bin/bac-games.sh" >> "$log" 2>&1
